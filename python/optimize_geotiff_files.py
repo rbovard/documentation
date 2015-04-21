44b38698bc80407804c06924de0c3759df28e9be
Rémi Bovard <remi.bovard@nyon.ch>
2015-04-21 11:15:17 +0200

    Add raster tileindex

--- python/optimize_geotiff_files.py
+++ python/optimize_geotiff_files.py
@@ -5,18 +5,22 @@ import subprocess
 
 inputPath = os.path.normpath("C:\Temp")
 outputPath = os.path.join(inputPath, "output")
+tileindexFile = os.path.join(outputPath, "tileindex.shp")
+inputRasterExtension = "tif"
+outputRasterExtension = "tif"
 
 if not os.path.exists(outputPath):
     os.mkdir(outputPath)
 
+# Convert to GeoTIFF
 for file in os.listdir(inputPath):
     inputFile = os.path.join(inputPath, file)
 
     if os.path.isfile(inputFile):
         fileName = file.split(".")
 
-        if fileName[1] == "tif":
-            outputFile = os.path.join(outputPath, file)
+        if fileName[1] == inputRasterExtension:
+            outputFile = os.path.join(outputPath, fileName[0] + "." + outputRasterExtension)
 
             # Create tiled file
             gdalCommand = "gdal_translate -of GTiff -co \"TILED=YES\" -co \"TFW=YES\" {} {}" . format(inputFile, outputFile)
@@ -25,3 +29,7 @@ for file in os.listdir(inputPath):
             # Build overview images
             gdalCommand = "gdaladdo -r average {} 2 4 8 16" . format(outputFile)
             process = subprocess.Popen(gdalCommand).communicate()[0]
+
+# Generate raster tileindex
+gdalCommand = "gdaltindex {} {}" . format(tileindexFile, os.path.join(outputPath, "*." + outputRasterExtension))
+output = subprocess.Popen(gdalCommand).communicate()[0]
