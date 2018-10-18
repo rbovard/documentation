ad11dd1c3d26094e97d9ed409b42a62bd9df2dc3
Rémi Bovard <remi.bovard@gmail.com>
2018-10-18 08:24:03 +0200

    Refactorize code

--- python/optimize_raster.py
+++ python/optimize_raster.py
@@ -3,35 +3,43 @@
 import os
 import subprocess
 
-inputPath = os.path.normpath(r"C:\Temp")
-outputPath = os.path.join(inputPath, "output")
-tileindexFile = os.path.join(outputPath, "tileindex.shp")
-inputRasterExtension = "tif"
-outputRasterExtension = "tif"
+''' Convert to GeoTIFF '''
+def convert_to_geotiff(input_path, input_raster_extension, output_path, output_compression):
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
 
-if not os.path.exists(outputPath):
-    os.mkdir(outputPath)
+    for file in os.listdir(input_path):
+        input_file = os.path.join(input_path, file)
 
-# Convert to GeoTIFF
-for file in os.listdir(inputPath):
-    inputFile = os.path.join(inputPath, file)
+        if os.path.isfile(input_file):
+            file_name = file.split(".")
 
-    if os.path.isfile(inputFile):
-        fileName = file.split(".")
+            if file_name[1] == input_raster_extension:
+                output_file = os.path.join(output_path, file_name[0] + ".tif")
 
-        if fileName[1] == inputRasterExtension:
-            outputFile = os.path.join(outputPath, fileName[0] + "." + outputRasterExtension)
+                # Create tiled file
+                gdal_command = "gdal_translate -of GTiff -co TILED=YES -co TFW=YES -co PROFILE=BASELINE -co COMPRESS={} {} {}".format(output_compression, input_file, output_file)
+                process = subprocess.Popen(gdal_command).communicate()[0]
 
-            # Create tiled file
-            # Use `COMPRESS=LZW` for lossless compression
-            # Use `COMPRESS=JPEG` for lossy compression
-            gdalCommand = "gdal_translate -of GTiff -co TILED=YES -co TFW=YES -co PROFILE=BASELINE -co COMPRESS=LZW {} {}".format(inputFile, outputFile)
-            process = subprocess.Popen(gdalCommand).communicate()[0]
+                # Build overview images
+                gdal_command = "gdaladdo -r average {} 2 4 8 16".format(output_file)
+                process = subprocess.Popen(gdal_command).communicate()[0]
 
-            # Build overview images
-            gdalCommand = "gdaladdo -r average {} 2 4 8 16".format(outputFile)
-            process = subprocess.Popen(gdalCommand).communicate()[0]
+''' Generate raster tileindex '''
+def generate_raster_tileindex(path):
+    tileindex_file = os.path.join(path, "tileindex.shp")
+    gdal_command = "gdaltindex {} {}".format(tileindex_file, os.path.join(path, "*.tif"))
+    output = subprocess.Popen(gdal_command).communicate()[0]
 
-# Generate raster tileindex
-gdalCommand = "gdaltindex {} {}".format(tileindexFile, os.path.join(outputPath, "*." + outputRasterExtension))
-output = subprocess.Popen(gdalCommand).communicate()[0]
+# Define variables
+input_path = os.path.normpath(r"C:\Temp")
+output_path = os.path.join(input_path, "output")
+input_raster_extension = "tif"
+
+# `LZW`: lossless compression
+# `JPEG`: lossy compression
+output_compression = "LZW"
+
+# Execute scripts
+convert_to_geotiff(input_path, input_raster_extension, output_path, output_compression)
+generate_raster_tileindex(output_path)
